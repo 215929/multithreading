bedfb9cd7bfedb4770da04d4eff431e724a91452
Zabielski.Wiktor <215929@edu.p.lodz.pl>
2020-06-08 18:43:40 +0200

    Added more tests, RepeatedTest annotation

--- mutlithreading/src/test/java/edu/iis/mto/multithread/RadarTest.java
+++ mutlithreading/src/test/java/edu/iis/mto/multithread/RadarTest.java
@@ -1,17 +1,41 @@
 package edu.iis.mto.multithread;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.Test;
 
 import static org.mockito.Mockito.*;
 
 class RadarTest {
 
-    @Test
+    PatriotBattery batteryMock;
+    Scud enemyMissle;
+
+    @BeforeEach
+    void init() {
+        batteryMock= mock(PatriotBattery.class);
+        enemyMissle = new Scud();
+    }
+
+    @RepeatedTest(10)
     void launchPatriotOnceWhenNoticesAScudMissle() {
-        PatriotBattery batteryMock = mock(PatriotBattery.class);
         BetterRadar radar = new BetterRadar(batteryMock, 1);
-        Scud enemyMissle = new Scud();
         radar.notice(enemyMissle);
         verify(batteryMock, timeout(100).times(1)).launchPatriot(enemyMissle);
     }
+
+    @RepeatedTest(10)
+    void launchPatriotTenTimesWhenNoticesAScudMissle() {
+        BetterRadar radar = new BetterRadar(batteryMock, 10);
+        radar.notice(enemyMissle);
+        verify(batteryMock, timeout(100).times(10)).launchPatriot(enemyMissle);
+    }
+
+    @RepeatedTest(10)
+    void launchPatriotZeroTimesWhenNoticesAScudMissle() {
+        BetterRadar radar = new BetterRadar(batteryMock, 0);
+        radar.notice(enemyMissle);
+        verify(batteryMock, timeout(100).times(0)).launchPatriot(enemyMissle);
+    }
+
 }
